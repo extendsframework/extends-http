e9f0d143304e0d33e9e8999dc79d0e6492f901ce
Vincent van Dijk <info@vincentvandijk.com>
2017-03-30 11:37:36 +0200

    Added not found middleware.

---
+++ src/Middleware/NotFound/NotFoundMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\Http\Middleware\NotFound;
+
+use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
+use ExtendsFramework\Http\Middleware\MiddlewareInterface;
+use ExtendsFramework\Http\Request\RequestInterface;
+use ExtendsFramework\Http\Response\Response;
+use ExtendsFramework\Http\Response\ResponseInterface;
+
+class NotFoundMiddleware implements MiddlewareInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function process(RequestInterface $request, MiddlewareChainInterface $chain): ResponseInterface
+    {
+        return (new Response())->withStatusCode(404);
+    }
+}
