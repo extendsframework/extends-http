a6aeeacb3aca015d5753c00df0247f002fe57566
Vincent van Dijk <info@vincentvandijk.com>
2017-12-08 07:50:26 +0100

    Removed renderer and middlewares, will be (temporary) placed in extends-application.

--- src/Framework/ServiceLocator/Loader/HttpConfigLoader.php
+++ src/Framework/ServiceLocator/Loader/HttpConfigLoader.php
@@ -3,20 +3,14 @@ declare(strict_types=1);
 
 namespace ExtendsFramework\Http\Framework\ServiceLocator\Loader;
 
-use ExtendsFramework\Http\Framework\Http\Middleware\Exception\ExceptionMiddleware;
-use ExtendsFramework\Http\Framework\Http\Middleware\Renderer\RendererMiddleware;
 use ExtendsFramework\Http\Framework\ServiceLocator\Factory\MiddlewareChainFactory;
 use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
-use ExtendsFramework\Http\Renderer\Json\JsonRenderer;
-use ExtendsFramework\Http\Renderer\RendererInterface;
 use ExtendsFramework\Http\Request\Request;
 use ExtendsFramework\Http\Request\RequestInterface;
 use ExtendsFramework\Http\Response\Response;
 use ExtendsFramework\Http\Response\ResponseInterface;
 use ExtendsFramework\ServiceLocator\Config\Loader\LoaderInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\FactoryResolver;
-use ExtendsFramework\ServiceLocator\Resolver\Invokable\InvokableResolver;
-use ExtendsFramework\ServiceLocator\Resolver\Reflection\ReflectionResolver;
 use ExtendsFramework\ServiceLocator\Resolver\StaticFactory\StaticFactoryResolver;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 
@@ -35,13 +29,6 @@ class HttpConfigLoader implements LoaderInterface
                 StaticFactoryResolver::class => [
                     RequestInterface::class => Request::class,
                     ResponseInterface::class => Response::class,
-                ],
-                InvokableResolver::class => [
-                    RendererInterface::class => JsonRenderer::class,
-                ],
-                ReflectionResolver::class => [
-                    RendererMiddleware::class => RendererMiddleware::class,
-                    ExceptionMiddleware::class => ExceptionMiddleware::class,
                 ],
             ],
         ];
--- test/Framework/ServiceLocator/Loader/HttpConfigLoaderTest.php
+++ test/Framework/ServiceLocator/Loader/HttpConfigLoaderTest.php
@@ -3,19 +3,13 @@ declare(strict_types=1);
 
 namespace ExtendsFramework\Http\Framework\ServiceLocator\Loader;
 
-use ExtendsFramework\Http\Framework\Http\Middleware\Exception\ExceptionMiddleware;
-use ExtendsFramework\Http\Framework\Http\Middleware\Renderer\RendererMiddleware;
 use ExtendsFramework\Http\Framework\ServiceLocator\Factory\MiddlewareChainFactory;
 use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
-use ExtendsFramework\Http\Renderer\Json\JsonRenderer;
-use ExtendsFramework\Http\Renderer\RendererInterface;
 use ExtendsFramework\Http\Request\Request;
 use ExtendsFramework\Http\Request\RequestInterface;
 use ExtendsFramework\Http\Response\Response;
 use ExtendsFramework\Http\Response\ResponseInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\FactoryResolver;
-use ExtendsFramework\ServiceLocator\Resolver\Invokable\InvokableResolver;
-use ExtendsFramework\ServiceLocator\Resolver\Reflection\ReflectionResolver;
 use ExtendsFramework\ServiceLocator\Resolver\StaticFactory\StaticFactoryResolver;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 use PHPUnit\Framework\TestCase;
@@ -41,13 +35,6 @@ class HttpConfigLoaderTest extends TestCase
                 StaticFactoryResolver::class => [
                     RequestInterface::class => Request::class,
                     ResponseInterface::class => Response::class,
-                ],
-                InvokableResolver::class => [
-                    RendererInterface::class => JsonRenderer::class,
-                ],
-                ReflectionResolver::class => [
-                    RendererMiddleware::class => RendererMiddleware::class,
-                    ExceptionMiddleware::class => ExceptionMiddleware::class,
                 ],
             ],
         ], $loader->load());
