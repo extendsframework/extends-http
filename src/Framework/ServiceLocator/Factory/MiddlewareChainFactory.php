3a151cf4918db3dbae5943e12ab745021338f3a8
Vincent van Dijk <info@vincentvandijk.com>
2017-12-27 14:53:46 +0100

    Changed return type.

--- src/Framework/ServiceLocator/Factory/MiddlewareChainFactory.php
+++ src/Framework/ServiceLocator/Factory/MiddlewareChainFactory.php
@@ -5,7 +5,9 @@ namespace ExtendsFramework\Http\Framework\ServiceLocator\Factory;
 
 use ExtendsFramework\Http\Middleware\Chain\MiddlewareChain;
 use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
+use ExtendsFramework\Http\Middleware\MiddlewareInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\ServiceFactoryInterface;
+use ExtendsFramework\ServiceLocator\ServiceLocatorException;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 
 class MiddlewareChainFactory implements ServiceFactoryInterface
@@ -13,18 +15,31 @@ class MiddlewareChainFactory implements ServiceFactoryInterface
     /**
      * @inheritDoc
      */
-    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): MiddlewareChainInterface
+    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): object
     {
         $config = $serviceLocator->getConfig();
 
         $chain = new MiddlewareChain();
         foreach ($config[MiddlewareChainInterface::class] ?? [] as $middleware => $priority) {
             $chain->addMiddleware(
-                $serviceLocator->getService($middleware),
+                $this->getMiddleware($serviceLocator, $middleware),
                 $priority
             );
         }
 
         return $chain;
+    }
+
+    /**
+     * Get middleware for key.
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param string                  $key
+     * @return MiddlewareInterface
+     * @throws ServiceLocatorException
+     */
+    protected function getMiddleware(ServiceLocatorInterface $serviceLocator, string $key): object
+    {
+        return $serviceLocator->getService($key);
     }
 }
